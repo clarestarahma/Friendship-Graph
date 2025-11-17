e095d7747d8289e4e69123839bd38b896cd1f8a8
shafiyyahdanis-ops <shafiyyahdanis@student.ub.ac.id>
2025-11-18 01:06:17 +0700

    Graph.java
    Signed-off-by: shafiyyahdanis-ops <shafiyyahdanis@student.ub.ac.id>

--- Graph.java
+++ Graph.java
@@ -1,3 +1,41 @@
 public class Graph {
-    
+
+    private int jumlahVertex;
+    private Node[] daftarNama;
+    private String[][] tabelNama;
+
+    public Graph(int jumlahVertex) {
+        this.jumlahVertex = jumlahVertex;
+        daftarNama = new Node[jumlahVertex];
+        tabelNama = new String[jumlahVertex][jumlahVertex];
+    }
+
+    public void setVertex(int index, String name) {
+        daftarNama[index] = new Node(name);
+    }
+
+    public void showAllVertex() {
+        System.out.println("=== Daftar Semua Vertex (Teman) ===");
+
+        for (int i = 0; i < jumlahVertex; i++) {
+            if (daftarNama[i] != null) {
+                System.out.println((i + 1) + ". " + daftarNama[i].getName());
+            }
+        }
+    }
+
+    public void showAdjacency() {
+        System.out.println("=== List Pertemanan ===");
+
+        for (int i = 0; i < jumlahVertex; i++) {
+            System.out.print(daftarNama[i].getName() + " : ");
+
+            for (int j = 0; j < tabelNama[i].length; j++) {
+                if (tabelNama[i][j] != null) {
+                    System.out.print(tabelNama[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
