9513925c6dab62bf4e31435e326be3eb247da9f1
shafiyyahdanis-ops <shafiyyahdanis@student.ub.ac.id>
2025-11-18 08:36:58 +0700

    Update Graph.java
    Signed-off-by: shafiyyahdanis-ops <shafiyyahdanis@student.ub.ac.id>

--- Graph.java
+++ Graph.java
@@ -1,18 +1,22 @@
-class Stack{
+class Stack {
     Node top;
-    Stack(){
+
+    Stack() {
         top = null;
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return top == null;
     }
-    public void push(String nama){
+
+    public void push(String nama) {
         Node node = new Node(nama);
         node.setNext(top);
-        top =  node;
+        top = node;
     }
-    public String pop(){
-        if(top == null) return null;
+
+    public String pop() {
+        if (top == null) return null;
         Node delete = top;
         top = top.getNext();
         return delete.getName();
@@ -20,74 +24,56 @@ class Stack{
 }
 
 public class Graph {
-    private String tabelData[][];
-    private Node listName[];
-    private int indexData;
-    private int jumlahVertex; // ini belum, harusnya tiap nambahin di increment, atau ngga perlu dijadiin atribut sih. Bisa buat method count vertex aja.
+    private String tabelData[][];   
+    private Node listName[];         
+    private int indexData;          
 
-    Graph(){
+    Graph() {
         listName = new Node[10];
         tabelData = new String[10][2];
         indexData = 0;
     }
 
-    private Node getNodeByName(String name){
+   
+    public int countVertex() {
+        int count = 0;
+        for (int i = 0; i < tabelData.length; i++) {
+            if (tabelData[i][0] != null) count++;
+        }
+        return count;
+    }
+
+    private Node getNodeByName(String name) {
         int index = getIndex(name);
+        if (index == -1) return null;
         return listName[index];
     }
 
-    public void dfs(String startNode){
-        Stack stack = new Stack();
-        int indexNode = getIndex(startNode);
-
-        if(indexNode == -1) return;
-
-        boolean[] visited = new boolean[listName.length];
-        Node current = listName[indexNode];
-        String name = current.getName();
-        stack.push(name);
-        
-        while (!stack.isEmpty()) {
-            name = stack.pop();
-            current = getNodeByName(name);
-            indexNode = getIndex(name);
-            if(!visited[indexNode]){
-                System.out.println(name);
-                visited[indexNode] = true;
-            }
-            Node afterCurrent = current.getNext();
-            String nameAfterCurrent;
-            while (afterCurrent != null) {
-                nameAfterCurrent = afterCurrent.getName();
-                indexNode = getIndex(nameAfterCurrent);
-                if(!visited[indexNode])
-                    stack.push(nameAfterCurrent);
-                afterCurrent = afterCurrent.getNext();
-            }
-        }
-    }
-
-    public int getIndex(String name){
+    public int getIndex(String name) {
         for (int i = 0; i < tabelData.length; i++) {
-            if(tabelData[i][0] != null){
-                if(tabelData[i][0].equals(name)) return Integer.parseInt(tabelData[i][1]);
+            if (tabelData[i][0] != null) {
+                if (tabelData[i][0].equals(name)) {
+                    return Integer.parseInt(tabelData[i][1]);
+                }
             }
         }
         return -1;
     }
 
-    public void resize(){
-        if(indexData == tabelData.length){
+    public void resize() {
+        if (indexData == tabelData.length) {
             String tempTabelData[][] = new String[tabelData.length * 2][tabelData[0].length];
             Node[] tempGraph = new Node[listName.length * 2];
+
             copyArray(tabelData, tempTabelData);
             copyArray(listName, tempGraph);
+
             tabelData = tempTabelData;
             listName = tempGraph;
         }
     }
 
-    private void copyArray(String[][] source, String[][] destination){
+    private void copyArray(String[][] source, String[][] destination) {
         for (int i = 0; i < source.length; i++) {
             for (int j = 0; j < source[i].length; j++) {
                 destination[i][j] = source[i][j];
@@ -95,16 +81,15 @@ public class Graph {
         }
     }
 
-    private void copyArray(Node[] source, Node[] destination){
+    private void copyArray(Node[] source, Node[] destination) {
         for (int i = 0; i < source.length; i++) {
             destination[i] = source[i];
         }
     }
-
-    public void showAllVertex() {
+   public void showAllVertex() {
         System.out.println("=== Daftar Semua Vertex (Teman) ===");
 
-        for (int i = 0; i < jumlahVertex; i++) { // pake for tapi dari tabelData aja
+        for (int i = 0; i < tabelData.length; i++) {
             if (listName[i] != null) {
                 System.out.println((i + 1) + ". " + listName[i].getName());
             }
@@ -112,17 +97,60 @@ public class Graph {
     }
 
     public void showAdjacency() {
-        System.out.println("=== List Pertemanan ===");
-
-        for (int i = 0; i < jumlahVertex; i++) { // ini juga pakai while aja sih, jangan terpaku sama jumlah vertex.
-            System.out.print(listName[i].getName() + " : ");
-
-            for (int j = 0; j < tabelData[i].length; j++) {
+     System.out.println("=== List Pertemanan ===");     
+        int i = 0;
+        while (i < tabelData.length) {
+            if (listName[i] != null) {
+                System.out.print(listName[i].getName() + " : ");
+            } else {
+                i++;
+                continue;
+            }
+        for (int j = 0; j < tabelData[i].length; j++) {
                 if (tabelData[i][j] != null) {
                     System.out.print(tabelData[i][j] + " ");
                 }
             }
+
             System.out.println();
+            i++;
+        }
+    }
+
+    
+    public void dfs(String startNode) {
+        Stack stack = new Stack();
+        int indexNode = getIndex(startNode);
+
+        if (indexNode == -1) return;
+
+        boolean[] visited = new boolean[listName.length];
+        Node current = listName[indexNode];
+        String name = current.getName();
+        stack.push(name);
+
+        while (!stack.isEmpty()) {
+            name = stack.pop();
+            current = getNodeByName(name);
+            indexNode = getIndex(name);
+
+            if (!visited[indexNode]) {
+                System.out.println(name);
+                visited[indexNode] = true;
+            }
+
+            Node afterCurrent = current.getNext();
+            String nameAfterCurrent;
+
+            while (afterCurrent != null) {
+                nameAfterCurrent = afterCurrent.getName();
+                indexNode = getIndex(nameAfterCurrent);
+
+                if (!visited[indexNode])
+                    stack.push(nameAfterCurrent);
+
+                afterCurrent = afterCurrent.getNext();
+            }
         }
     }
 }
